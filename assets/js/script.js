866d9552bee1da0bae2b814b0b4b9604765f5207
nezjutsu <seankral9@gmail.com>
2022-01-02 21:10:27 -0500

    display data from api call for current weather, use moment.js to create dates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,21 @@
-var cityFormEl = document.querySelector("#city-form")
-var cityInputEl = document.querySelector("#form-input")
-var currentWeatherEl = document.querySelector("#current-weather")
+var cityFormEl = document.querySelector("#city-form");
+var cityInputEl = document.querySelector("#form-input");
+var currentWeatherEl = document.querySelector("#current-weather");
+
+var WeatherArrText = ["Temp: ", "Wind: ", "Humidity: ", "UV Index: "]
+var currentWeatherArr = [];
+
+var forecastDates = []
+
+var currentDay = moment().format('M/D/YYYY')
+
+//for (var i = 0; i < 4; i++ ) {
+    //var dates = moment().add(1, 'days')
+    //forecastDates.push(dates)
+//}
+
+//console.log(forecastDates)
+
 
 var formSubmitHandler = function (e) {
     e.preventDefault();
@@ -13,11 +28,15 @@ var formSubmitHandler = function (e) {
     }
 
     cityFormEl.reset();
+
+    // clear current content
+    currentWeatherEl.innerHTML = "";
+
 }
 
 var getCityCords = function (city) {
 
-    // format dynamic api url for lat and long
+    // create dynamic api url for lat and long
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=c4d612ec642b20bf888ccab96f3082e7"
 
     // make api request
@@ -26,23 +45,25 @@ var getCityCords = function (city) {
         if (response.ok) {
             response.json().then(function (data) {
                 // pass lat and long to new api call
-                getCurrentWeather(data.coord.lat, data.coord.lon);
+                getCurrentWeather(data.coord.lat, data.coord.lon, data.name);
                 console.log(data)
             });
         }
         else {
             alert("Error: City Not Found.")
+            currentWeatherEl.innerHTML = "";
         };
     })
+        // if server error
         .catch(function (error) {
             alert("Unable to connect to OpenWeatherMap")
         });
 };
 
-var getCurrentWeather = function (lat, lon) {
-    
+var getCurrentWeather = function (lat, lon, city) {
+
     // create dynamic url using lat and lon
-    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly,alerts&appid=c4d612ec642b20bf888ccab96f3082e7"
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&exclude=minutely,hourly,alerts&appid=c4d612ec642b20bf888ccab96f3082e7"
 
     // make api request
     fetch(apiUrl).then(function (response) {
@@ -50,20 +71,43 @@ var getCurrentWeather = function (lat, lon) {
         if (response.ok) {
             response.json().then(function (data) {
                 console.log(data)
+                // push needed data to empty array
+                currentWeatherArr = [];
+                currentWeatherArr.push(data.current.temp, data.current.wind_speed + " MPH", data.current.humidity + " %", data.current.uvi)
+                displayWeather();
             });
         }
         else {
             alert("Error: City Not Found.")
         };
     })
+        // if server error
         .catch(function (error) {
             alert("Unable to connect to OpenWeatherMap")
         });
+
+        // display name of city returned from api call
+        var cityNameEl = document.createElement("h2");
+        cityNameEl.className = "city-header";
+        cityNameEl.textContent = city
+        currentWeatherEl.appendChild(cityNameEl);
 };
 
 var displayWeather = function () {
-    
-    
+    console.log(currentWeatherArr)
+
+    var currentUl = document.createElement("ul");
+    currentUl.className = "currentUl"
+    currentWeatherEl.appendChild(currentUl);
+
+    for (var i = 0; i < currentWeatherArr.length; i++) {
+
+        // create list elements
+        var currentLi = document.createElement("li")
+        currentLi.className = "currentLi"
+        currentLi.textContent = WeatherArrText[i] + currentWeatherArr[i]
+        currentUl.appendChild(currentLi);
+    };
 }
 
 cityFormEl.addEventListener("submit", formSubmitHandler)
